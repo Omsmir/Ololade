66f507e99be874037f87cc037f0410039cd0dc54
OmarSamir <omarsamiir@outlook.com>
2022-07-17 01:05:09 +0200

    New Items

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 let li = document.querySelector("ul").querySelectorAll("li a")
 
+let dark = document.querySelector(".items .dark")
+
+let h1 = document.querySelectorAll("h1")
+
+let p = document.querySelectorAll("p")
+
+let btn = document.querySelectorAll(".button")
 
     li.forEach((ele) =>{
         ele.addEventListener("click",()=>{
@@ -15,3 +22,33 @@ let li = document.querySelector("ul").querySelectorAll("li a")
         })
 
     })
+
+dark.addEventListener("click",()=>{
+    if(dark.classList.contains("dark")){
+        li.forEach((ele)=>{
+            if(ele.classList.contains("active")){
+                return true
+            }else{
+                ele.classList.add("night")
+            }       
+        })
+        document.body.classList.add("light")
+        h1.forEach((ele)=>{
+            if(ele.classList.contains("night")){
+                ele.classList.remove("night")
+
+            }else {
+                ele.classList.remove("light")
+                ele.classList.add("night")
+
+            }
+        })
+
+        p.forEach((ele)=>{
+            ele.classList.add("light")
+        })
+        
+    }else {
+
+    }
+})
